231be9ebc4f00e7a0cf67e837cf1349b87fe455b
Niladri-B <nilan666@gmail.com>
2018-07-13 16:31:20 +0200

    Adding latest rosalind script

---
+++ Rosalind/perl_rosaline.pl
@@ -0,0 +1,46 @@
+use 5.014;
+use warnings;
+
+my $string = 'ACGCCAAAACGCCAAGACACGCCAAACGCCAACTGCAACGCCAACACGCCAAACGCCAAACGCCAAACGCCAAGGTTCCTGCCTCGCAACACACGCCAAACGCCAACACGTACGCCAATAATGACGACGCCAACTACGCCAAACGCCAAAGACGCCAACACGCCAACACGCCAACACGCCAAACGCCAATCACGCCAAACGCCAACACATCTTCCACGCCAATTCTGTTCGACGCCAACTTTTACGCCAAGAGACGCCAAAAACGCCAATCAGCGTTTATTATAACGCCAAACGCCAAAGTGATCACGCCAAAACGCCAATACACGCCAAACGCCAAACGCCAAGAACGCCAAACGCCAATCCGTCAGAAACGCCAAACGCCAACTGCGCGGGGGACATACGCCAATACGCCAAGACGCCAAGTACGCCAAACGCCAATACGCCAATACGCCAAACGCCAAACGCCAAACGCCAACTCCTTACGCCAAAGAACGCCAAACGCCAAACGCCAATACGTACGCCAATAGTCACGCCAAACGCCAAACGCCAAGCTACGCCAATTGACGCCAAGTCACGCCAACACAACCCTGACCACGCCAACGCACGCCAATACACAACGATATACGCCAAATACGCCAAAGCTAACGCCAAGTTTCACGCCAAGAGGAACTCACGCCAATGCACGCCAAGTGACGCCAAACGCCAACTGACGCCAACTACGCCAACAAACGCCAACCGCACTACGTACGCCAAGACGCCAACCACGCCAATTTACGCCAAGAACGCCAATACGCCAATACGCCAAT';
+#'GATATATGCATATACTT';
+
+my $sub = 'ACGCCAAAC';
+#'AT' ;
+#'ATAT';
+my $sub_len = length $sub;
+
+#say length $sub;
+
+my @string_split = split '', $string;
+# say "@string_split";
+my $string_split_length = @string_split;
+# say "Length of string : $string_split_length";
+
+# for (my $i = 0; $i <= $string_split_length; $i++){
+# 	#say $string_split[$i];
+# 	push my @arr, $string_split[$i];
+# 	say @arr;
+# }
+
+# my @arr;
+# foreach (@string_split){
+# 	push @arr,$_;
+# 	say @arr;
+# }
+
+# my $return = substr($string, 0, 4);
+# say $return;
+# my $return = substr($string, 1, 4);
+# say $return;
+# my $return = substr($string, 2, 4);
+# say $return;
+# my $return = substr($string, 3, 4);
+# say $return;
+# my $return = substr($string, 4, 4);
+# say $return;
+
+for (my $i = 0; $i <= $string_split_length - $sub_len; $i++){
+	my $return = substr($string, $i, $sub_len);
+	#say $return,",", $i+1 if $return eq $sub;
+	say $i+1 if $return eq $sub;
+}
